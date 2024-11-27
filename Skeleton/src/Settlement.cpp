e077f6b986fdb899c312442db00966144598ce68
Legit-source <tomnev@post.bgu.ac.il>
2024-11-27 20:39:38 +0000

    fixed settlement.cpp (im going to kill you yulia)

--- Skeleton/src/Settlement.cpp
+++ Skeleton/src/Settlement.cpp
@@ -1,71 +1,45 @@
-/**
- * @file Settlement.h
- * @brief Defines the Settlement class and related enumerations.
- */
-
 #pragma once
 #include <string>
 #include <vector>
-using std::string;
-using std::vector;
-
-class Facility;
-
-/**
- * @enum SettlementType
- * @brief Enumeration representing different types of settlements.
- */
-enum class SettlementType
-{
-    VILLAGE,    /**< Represents a village settlement type. */
-    CITY,       /**< Represents a city settlement type. */
-    METROPOLIS, /**< Represents a metropolis settlement type. */
-};
+#include "Settlement.h"
+using namespace std;
 
 /**
  * @class Settlement
  * @brief Represents a settlement with a name and type.
  */
-class Settlement
-{
-public:
-    /**
-     * @brief Constructs a Settlement object.
-     * @param name The name of the settlement.
-     * @param type The type of the settlement.
-     */
-    Settlement(const string &name, SettlementType type)
-    : name(name), type(type){};
 
-    /**
-     * @brief Gets the name of the settlement.
-     * @return The name of the settlement.
-     */
-    const string &getName() const{
-        return name;
-    };
+/**
+ * @brief Constructs a Settlement object.
+ * @param name The name of the settlement.
+ * @param type The type of the settlement.
+ */
+Settlement::Settlement(const string &name, SettlementType type)
+: name(name), type(type){};
 
-    /**
-     * @brief Gets the type of the settlement.
-     * @return The type of the settlement.
-     */
-    SettlementType getType() const{
-        return type;
-    };
-
-    /**
-     * @brief Converts the settlement information to a string.
-     * @return A string representation of the settlement.
-     */
-    const string toString() const{
-        switch (type) {
-            case SettlementType::VILLAGE: return "SettlementName: " + name + "\nSettlementType: Village";
-            case SettlementType::CITY: return "SettlementName: " + name + "\nSettlementType: City";
-            case SettlementType::METROPOLIS: return "SettlementName: " + name + "\nSettlementType: Metropolis";
-    }
-    };
-
-private:
-    const string name;   /**< The name of the settlement. */
-    SettlementType type; /**< The type of the settlement. */
+/**
+ * @brief Gets the name of the settlement.
+ * @return The name of the settlement.
+ */
+const string Settlement::&getName() const{
+    return name;
 };
+
+/**
+ * @brief Gets the type of the settlement.
+ * @return The type of the settlement.
+ */
+Settlement::SettlementType getType() const{
+    return type;
+};
+
+/**
+ * @brief Converts the settlement information to a string.
+ * @return A string representation of the settlement.
+ */
+const string Settlement::toString() const{
+    switch (type) {
+        case SettlementType::VILLAGE: return "SettlementName: " + name + "\nSettlementType: Village";
+        case SettlementType::CITY: return "SettlementName: " + name + "\nSettlementType: City";
+        case SettlementType::METROPOLIS: return "SettlementName: " + name + "\nSettlementType: Metropolis";
+}
