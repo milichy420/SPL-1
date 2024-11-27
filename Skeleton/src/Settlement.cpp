7dcb100e6519cdeebba6d3b59dba7602427833b8
Legit-source <tomnev@post.bgu.ac.il>
2024-11-27 19:51:11 +0000

    added printstatus() and addsettlemenet() with all their required subfunctions

--- Skeleton/src/Settlement.cpp
+++ Skeleton/src/Settlement.cpp
@@ -34,25 +34,36 @@ public:
      * @param name The name of the settlement.
      * @param type The type of the settlement.
      */
-    Settlement(const string &name, SettlementType type);
+    Settlement(const string &name, SettlementType type)
+    : name(name), type(type){};
 
     /**
      * @brief Gets the name of the settlement.
      * @return The name of the settlement.
      */
-    const string &getName() const;
+    const string &getName() const{
+        return name;
+    };
 
     /**
      * @brief Gets the type of the settlement.
      * @return The type of the settlement.
      */
-    SettlementType getType() const;
+    SettlementType getType() const{
+        return type;
+    };
 
     /**
      * @brief Converts the settlement information to a string.
      * @return A string representation of the settlement.
      */
-    const string toString() const;
+    const string toString() const{
+        switch (type) {
+            case SettlementType::VILLAGE: return "SettlementName: " + name + "\nSettlementType: Village";
+            case SettlementType::CITY: return "SettlementName: " + name + "\nSettlementType: City";
+            case SettlementType::METROPOLIS: return "SettlementName: " + name + "\nSettlementType: Metropolis";
+    }
+    };
 
 private:
     const string name;   /**< The name of the settlement. */
